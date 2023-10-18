baa530c0d47c9a6822073261936ba5971cb280da
woozlic <sampaccs@mail.ru>
2023-10-18 12:00:47 +0500

    HW2 is completed

--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -16,6 +16,10 @@ func TestUnpack(t *testing.T) {
 		{input: "abccd", expected: "abccd"},
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
+		{input: "a0xyz3", expected: "xyzzz"},
+		{input: "ban2ed", expected: "banned"},
+		{input: "triplesix3", expected: "triplesixxx"},
+		{input: "relax, you are doing fine", expected: "relax, you are doing fine"},
 		// uncomment if task with asterisk completed
 		// {input: `qwe\4\5`, expected: `qwe45`},
 		// {input: `qwe\45`, expected: `qwe44444`},
